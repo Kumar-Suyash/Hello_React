3871657a032791d35d4b0f1939f2478aadfa3c6c
kumar Suyash <ksuyash001@gmail.com>
2026-05-14 16:53:08 +0530

    Refactor app.js to use JSX for rendering and update index.html structure; add build script to package.json

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello world from react");
+//* React.createElement => React element =(JS Object) => HTML element
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello world from react",
+);
+
+//* JSX (transpiled it before it reaches the JS ) = Parcel + Babel => React.createElement => React element (JS Object) => HTML element
+
+const jsxHeading = (
+  <h1 id="heading" xyz="abc">
+    Hello world from react through JSX
+  </h1>
+);
 
 console.log(heading); //* Object
+console.log(jsxHeading); //* Object
+//* so heading and jsxHeading are same
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
+root.render(jsxHeading);
