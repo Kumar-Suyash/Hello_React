0a4ad6fd242b8660e1b3033a01bd827870b1be10
kumar Suyash <ksuyash001@gmail.com>
2026-06-18 14:47:52 +0530

    Add filter button to Body component for top-rated restaurants; update state management to filter restaurant list based on rating

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,17 +2,31 @@ import React from "react";
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/mockData";
 import { RESTAURANT_IMAGES, FALLBACK_IMG } from "../utils/constants";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
   return (
     <div className="body">
       <div className="search-container">
         <input type="text" placeholder="Search for restaurants or dishes" />
         <button>Search</button>
       </div>
+      <div
+        className="filter-btn"
+        onClick={() => {
+          const filteredlist = listOfRestaurants.filter(
+            (restaurant) => restaurant.info.avgRating > 4.5,
+          );
+          setListOfRestaurants(filteredlist);
+        }}
+      >
+        <button>Top Rated Resturants</button>
+      </div>
 
       <div className="restaurant-list">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           // key must be unique — using the API's own id field
           <RestaurantCard key={restaurant.info.id} resData={restaurant.info} />
         ))}
