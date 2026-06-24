2d5290b84a18a40a5407e96f65e715a4bc34e681
kumar Suyash <ksuyash001@gmail.com>
2026-06-24 18:36:00 +0530

    Implement routing with react-router; add About, Contact, and Error components; update Header for navigation links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [btnName, setBtnName] = React.useState("Login");
+  const [btnName, setBtnName] = useState("Login");
+  console.log("Header rendered");
+
+  //* if no dependency array => useEffect will be called on every render
+  //* if dependency array is empty = [] => useEffect will be called only once after initial render(just once)
+  //* if dependency array has some state variable [btnName] => useEffect will be called only when that state variable changes.
+
+  useEffect(() => {
+    console.log("useEffect called");
+  }, [btnName]);
   return (
     <div className="header">
       <div className="logo-container">
@@ -13,10 +23,16 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact Us</li>
-          <li>🛒 Cart</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>🛒 Cart </li>
           <button
             className="login-btn"
             onClick={() => setBtnName(btnName === "Login" ? "Logout" : "Login")}
